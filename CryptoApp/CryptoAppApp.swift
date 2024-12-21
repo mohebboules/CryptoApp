c1a8cf787d7ba9e2d006e272cb201ef2502c08ff
Moheb Boules Guirges <mohebboules@gmail.com>
2024-12-21 16:32:26 +0200

    Initial Commit

---
+++ CryptoApp/CryptoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CryptoAppApp.swift
+//  CryptoApp
+//
+//  Created by Moheb Boules on 21/12/2024.
+//
+
+import SwiftUI
+
+@main
+struct CryptoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
