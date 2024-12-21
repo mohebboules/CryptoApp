ed383554cfb8b3ab0f460c9c6449e25d85230143
Moheb Boules Guirges <mohebboules@gmail.com>
2024-12-21 17:35:12 +0200

    Adding costume header

---
+++ core/Home/Views/HomeView.swift
@@ -0,0 +1,18 @@
+//
+//  HomeView.swift
+//  CryptoApp
+//
+//  Created by Moheb Boules on 21/12/2024.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    HomeView()
+}
