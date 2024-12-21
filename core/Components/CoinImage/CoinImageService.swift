0f25e54ff5abed133f8855ecf2d7bf849a2f4d9d
Moheb Boules Guirges <mohebboules@gmail.com>
2024-12-21 22:00:24 +0200

    Saving image to file manager

--- core/Components/CoinImage/CoinImageService.swift
+++ core/Components/CoinImage/CoinImageService.swift
@@ -13,11 +13,24 @@ class CoinImageService {
     @Published var image: UIImage? = nil
     private var imageSubscribtion: AnyCancellable?
     private let coin: CoinModel
+    private let fileManager = LocalFileManager.instance
+    private let folderName: String = "coin_images"
+    private let imageName: String
     init(coin: CoinModel){
         self.coin = coin
+        self.imageName = coin.id
         getCoinImage()
+        
     }
     private func getCoinImage(){
+        if let savedImage = fileManager.getImage(imageName: imageName, folderName: folderName) {
+            image = savedImage
+        } else {
+            downloadCoinImage()
+        }
+        
+    }
+    private func downloadCoinImage(){
         guard let url = URL(string: coin.image) else { return }
         imageSubscribtion = NetworkingManager.download(url: url)
             .tryMap({ (data) -> UIImage? in
@@ -25,9 +38,12 @@ class CoinImageService {
             })
 
             .sink(receiveCompletion: NetworkingManager.handleCompletion, receiveValue: { [weak self] (returnedImage) in
-                self?.image = returnedImage
-                self?.imageSubscribtion?.cancel()
+                guard let self = self, let downloadedImage = returnedImage else { return }
+                self.image = downloadedImage
+                self.imageSubscribtion?.cancel()
+                self.fileManager.saveImage(image: downloadedImage, imageName: self.imageName, folderName: self.folderName)
             })
         
     }
+    
 }
