cfa0100d64badb391897c5c71e897652d276cad5
Moheb Boules Guirges <mohebboules@gmail.com>
2024-12-21 23:50:32 +0200

    Adding search bar functionallity

--- core/Home/ViewModels/HomeViewModel.swift
+++ core/Home/ViewModels/HomeViewModel.swift
@@ -9,9 +9,12 @@ import Foundation
 import Combine
 
 class HomeViewModel: ObservableObject {
+    
     @Published var allCoins: [CoinModel] = []
     @Published var portofolioCoins: [CoinModel] = []
+    
     @Published var searchText: String = ""
+    
     private var cancellables: Set<AnyCancellable> = []
     
     private let dataService = CoinDataService()
@@ -21,11 +24,33 @@ class HomeViewModel: ObservableObject {
     }
     
     func addSubscribers(){
+        // If no search was implemented
+        /*
         dataService.$allCoins
             .sink { [weak self] (returnedCoins) in
                 self?.allCoins = returnedCoins
             }
             .store(in: &cancellables)
+         */
+        // Updates all coins (no need for the dataservice code due to the usage of combineLatest)
+        $searchText
+            .combineLatest(dataService.$allCoins)
+            .debounce(for: .seconds(0.2), scheduler: DispatchQueue.main)
+            .map(filterCoins)
+            .sink { [weak self] returnedCoins in
+                self?.allCoins = returnedCoins
+            }
+            .store(in: &cancellables)
+           
+    }
+    private func filterCoins(text: String, coins: [CoinModel]) -> [CoinModel]{
+        guard !text.isEmpty else { return coins }
+        let lowercasedText = text.lowercased()
+        return coins.filter { (coin) -> Bool in
+            return coin.name.lowercased().contains(lowercasedText) ||
+            coin.symbol.lowercased().contains(lowercasedText) ||
+            coin.id.lowercased().contains(lowercasedText)
+        }
     }
     
 }
